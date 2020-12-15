29f3c373173166c186ab30c09fd82705079121b3
EugeneYaremenko <yaremenko.e.s@gmail.com>
2020-12-15 20:11:16 +0200

    Updates

---
+++ precache-manifest.b6d58fc7e24b2b0523339e7ad73ef7e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20db83f575e8489c9abea0a84484bfdd",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "259c99bc1667b1dcece9",
+    "url": "/goit-react-hw-08-phonebook/static/css/3.d71e118b.chunk.css"
+  },
+  {
+    "revision": "d0907dde35404c4087e1",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.289fc766.chunk.css"
+  },
+  {
+    "revision": "e54fb8019ca1885e01cb",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.56af08d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.56af08d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259c99bc1667b1dcece9",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.04d324bb.chunk.js"
+  },
+  {
+    "revision": "dd5e7fda3a77277d6f66",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.715ad980.chunk.js"
+  },
+  {
+    "revision": "6ee353a8efd5adff87d8",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.01613940.chunk.js"
+  },
+  {
+    "revision": "7eec0ad64561447cfdf3",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.1e378974.chunk.js"
+  },
+  {
+    "revision": "c4c0cad3b4c98d282415",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.407680ed.chunk.js"
+  },
+  {
+    "revision": "d0907dde35404c4087e1",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.3ef709c8.chunk.js"
+  },
+  {
+    "revision": "10e00b1c5ecc0d395d4a",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.e10d586e.js"
+  }
+]);
---
+++ static/js/7.407680ed.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[7],{139:function(e,t,n){"use strict";n.r(t);var o=n(0),a=n.n(o);t.default=function(){return a.a.createElement("div",null,a.a.createElement("h1",null,"Home page"))}}}]);
+//# sourceMappingURL=7.407680ed.chunk.js.map
